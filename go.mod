244cd0bd140d0249b80576c3459849983cd50538
Zach Kelling <z@zeekay.ai>
2026-01-09 15:21:02 -0800

    refactor: migrate utils/hashing to crypto/hash

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/luxfi/platformvm
+
+go 1.25.5
+
+require (
+	github.com/luxfi/codec v1.1.2
+	github.com/luxfi/consensus v1.22.51
+	github.com/luxfi/constants v1.4.2
+	github.com/luxfi/crypto v1.17.38
+	github.com/luxfi/database v1.17.35
+	github.com/luxfi/genesis v1.5.21
+	github.com/luxfi/ids v1.2.8
+	github.com/luxfi/log v1.2.1
+	github.com/luxfi/math v1.2.2
+	github.com/luxfi/p2p v1.18.7
+	github.com/luxfi/rpc v1.0.0
+	github.com/luxfi/staking v1.0.0
+	github.com/luxfi/upgrade v1.0.0
+	github.com/luxfi/utils v1.1.0
+	github.com/luxfi/vm v1.0.15
+	github.com/luxfi/warp v1.18.2
+	github.com/stretchr/testify v1.11.1
+	google.golang.org/protobuf v1.36.11
+)
---
+++ txs/executor/backend.go
@@ -0,0 +1,35 @@
+// Copyright (C) 2019-2025, Lux Industries, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package executor
+
+import (
+	consensusctx "github.com/luxfi/consensus/context"
+	"github.com/luxfi/consensus/validator/uptime"
+	"github.com/luxfi/ids"
+	"github.com/luxfi/log"
+	"github.com/luxfi/platformvm/config"
+	"github.com/luxfi/platformvm/reward"
+	"github.com/luxfi/platformvm/utxo"
+	"github.com/luxfi/timer/mockable"
+	"github.com/luxfi/utils"
+	"github.com/luxfi/vm/platformvm/fx"
+)
+
+type Backend struct {
+	Config       *config.Internal
+	Ctx          *consensusctx.Context
+	Clk          *mockable.Clock
+	Fx           fx.Fx
+	FlowChecker  utxo.Verifier
+	Uptimes      uptime.Calculator
+	Rewards      reward.Calculator
+	Bootstrapped *utils.Atomic[bool]
+	Log          log.Logger
+}
+
+// SharedMemory provides cross-chain atomic operations
+type SharedMemory interface {
+	Get(peerChainID ids.ID, keys [][]byte) ([][]byte, error)
+	Apply(requests map[ids.ID]interface{}, batch ...interface{}) error
+}
